80f5ccf39a4283d198d7fd674cff079c8dbb9667
Max <maxnatanaelstk@gmail.com>
2025-09-13 00:56:50 -0300

    removing old unused features

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:latom/ui/pages/anime_by_id_screen.dart';
 import 'package:latom/ui/pages/guess_anime_screen.dart';
@@ -17,14 +16,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return LtScaffold(
@@ -77,19 +68,9 @@ class _HomePageState extends State<HomePage> {
                 );
               }
             ),
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      )
     );
   }
 }
